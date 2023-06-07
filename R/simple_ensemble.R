783e936e6d36b0d119d94a15e780b4ad19363841
Evan Ray <elray@umass.edu>
2023-06-07 14:48:01 -0400

    misc updates

--- R/simple_ensemble.R
+++ R/simple_ensemble.R
@@ -1,88 +1,85 @@
-#' Compute ensemble predictions by summarizing component predictions for each
-#' combination of model task, output type, and output type id. Supported output
-#' types include `mean`, `median`, `quantile`, `cdf`, and `pmf`.
+#' Compute ensemble model outputs by summarizing component model outputs for
+#' each combination of model task, output type, and output type id. Supported
+#' output types include `mean`, `median`, `quantile`, `cdf`, and `pmf`.
 #'
-#' @param predictions a `data.frame` with component model predictions. It should
-#'   have columns `team_abbr`, `model_abbr`, one column for each task id
-#'   variable, `output_type`, `output_type_id`, and `value`.
-#' @param task_id_cols an optional `character` vector naming the columns of
-#'   `predictions` that correspond to task id variables. The default is `NULL`,
-#'   in which case all columns in `predictions` _other than_ `team_abbr`,
-#'   `model_abbr`, and the specified `output_type_col`, `output_id_col`, and
-#'   `value_col` are used as task id variables.
+#' @param model_outputs an object of class `model_output_df` with component
+#'   model outputs (e.g., predictions).
 #' @param weights an optional `data.frame` with component model weights. If
-#'   provided, it should have columns `team_name`, `model_abbr`, `weight`,
-#'   and optionally, additional columns corresponding to task id variables,
-#'   `output_type`, or `output_id`, if weights are specific to values of
-#'   those variables. The default is `NULL`, in which case an equally-weighted
-#'   ensemble is calculated.
+#'   provided, it should have columns `model_id`, `weight`, and optionally,
+#'   additional columns corresponding to task id variables, `output_type`, or
+#'   `output_type_id`, if weights are specific to values of those variables. The
+#'   default is `NULL`, in which case an equally-weighted ensemble is calculated.
 #' @param agg_fun a function or character string name of a function to use for
-#'   aggregating component predictions into the ensemble prediction. The default
-#'   is `mean`, in which case the ensemble prediction is the simple average of
-#'   the component model predictions. The provided function should have an
-#'   argument `x` for the vector of numeric values to summarize, and for
-#'   weighted methods, an argument `w` with a numeric vector of weights.
+#'   aggregating component model outputs into the ensemble outputs. See the
+#'   details for more information.
 #' @param agg_args a named list of any additional arguments that will be passed
 #'   to `agg_fun`.
-#' @param team_abbr, model_abbr `character` strings with the name of the team
-#'   and model to use for the ensemble predictions.
-#' @param output_type_col, output_id_col, value_col `character` strings with the
-#'   names of the columns in `predictions` for the output's type, additional
-#'   identifying information, and value (of the prediction)
+#' @param model_id `character` string with the identifier to use for the
+#'   ensemble model.
+#' @param task_id_cols, output_type_col, output_type_id_col, value_col
+#'   `character` vectors with the names of the columns in `model_outputs` for
+#'   the output's type, additional identifying information, and value of the
+#'   model output.
+#'
+#' @details The default for `agg_fun` is `mean`, in which case the ensemble's
+#'   output is the average of the component model outputs within each group
+#'   defined by a combination of values in the task id columns, output type, and
+#'   output type id. The provided `agg_fun` should have an argument `x` for the
+#'   vector of numeric values to summarize, and for weighted methods, an
+#'   argument `w` with a numeric vector of weights. For weighted methods,
+#'  `agg_fun = "mean"` and `agg_fun = "median"` are translated to use
+#'  `matrixStats::weightedMean` and `matrixStats::weightedMedian` respectively.
 #'
 #' @return a data.frame with columns `team_abbr`, `model_abbr`, one column for
-#'   each task id variable, `output_type`, `output_id`, and `value`.
-simple_ensemble <- function(predictions, weights = NULL,
+#'   each task id variable, `output_type`, `output_id`, and `value`. Note that
+#'   any additional columns in the input `model_outputs` are dropped.
+simple_ensemble <- function(model_outputs, weights = NULL,
                             agg_fun = "mean", agg_args = list(),
-                            team_abbr = "hub", model_abbr = "ensemble",
+                            model_id = "hub-ensemble",
                             task_id_cols = NULL,
                             output_type_col = "output_type",
-                            output_id_col = "output_type_id",
+                            output_type_id_col = "output_type_id",
                             hub_connection = NULL) {
-  if (!is.data.frame(predictions)) {
-    cli::cli_abort(c("x" = "{.arg predictions} must be a `data.frame`."))
+  model_out_cols <- colnames(model_outputs)
+  if (!is.data.frame(model_outputs)) {
+    cli::cli_abort(c("x" = "{.arg model_outputs} must be a `data.frame`."))
   }
 
+  non_task_cols <- c("model_id", output_type_col, output_type_id_col, "value")
   if (is.null(task_id_cols)) {
-    cols <- colnames(predictions)
-    non_task_cols <- c("team_abbr", "model_abbr", output_type_col,
-                       output_id_col, "value")
-    task_id_cols <- cols[!cols %in% non_task_cols]
+    task_id_cols <- model_out_cols[!model_out_cols %in% non_task_cols]
   }
 
-  col_names <- c("team_abbr", "model_abbr", task_id_cols, output_type_col,
-                 output_id_col, "value")
-  if (!all(colnames(predictions) %in% col_names)) {
+  req_col_names <- c(non_task_cols, task_id_cols)
+  if (!all(req_col_names %in% model_out_cols)) {
     cli::cli_abort(c(
-      "x" = "{.arg predictions} did not have all required columns
-             {.val {col_names}}."
+      "x" = "{.arg model_outputs} did not have all required columns
+             {.val {req_col_names}}."
     ))
   }
-  
-  ## Validations above this point to be relocated to hubUtils
 
-  if (nrow(predictions) == 0) {
-    cli::cli_warn(c("!" = "{.arg predictions} has zero rows."))
+  ## Validations above this point to be relocated to hubUtils
+  # hubUtils::validate_model_output_df(model_outputs)
+
+  if (nrow(model_outputs) == 0) {
+    cli::cli_warn(c("!" = "{.arg model_outputs} has zero rows."))
   }
 
   valid_types <- c("mean", "median", "quantile", "cdf", "pmf")
-  unique_types <- unique(predictions[[output_type_col]])
+  unique_types <- unique(model_outputs[[output_type_col]])
   invalid_types <- unique_types[!unique_types %in% valid_types]
   if (length(invalid_types) > 0) {
     cli::cli_abort(c(
-      "x" = "{.arg predictions} contains unsupported output type.",
+      "x" = "{.arg model_outputs} contains unsupported output type.",
       "i" = "Included output type{?s}: {.val {invalid_types}}.",
       "i" = "Supported output types: {.val {valid_types}}."
     ))
   }
 
   if (is.null(weights)) {
-    if (agg_fun == "mean") agg_fun <- mean
-    if (agg_fun == "median") agg_fun <- median
-
-    agg_args <- c(agg_args, list(x = "value"))
+    agg_args <- c(agg_args, list(x = quote(.data[["value"]])))
   } else {
-    req_weight_cols <- c("model_abbr", "team_abbr", "weight")
+    req_weight_cols <- c("model_id", "weight")
     if (!all(req_weight_cols %in% colnames(weights))) {
       cli::cli_abort(c(
         "x" = "{.arg weights} did not include required columns
@@ -99,38 +96,45 @@ simple_ensemble <- function(predictions, weights = NULL,
       ))
     }
 
-    invalid_cols <- weight_by_cols[!weight_by_cols %in% colnames(predictions)]
-    if (!all(weight_by_cols %in% colnames(predictions))) {
+    invalid_cols <- weight_by_cols[!weight_by_cols %in% colnames(model_outputs)]
+    if (!all(weight_by_cols %in% colnames(model_outputs))) {
       cli::cli_abort(c(
         "x" = "{.arg weights} included {length(invalid_cols)} column{?s} that
-               {?was/were} not present in {.arg predictions}:
+               {?was/were} not present in {.arg model_outputs}:
                {.val {invalid_cols}}"
       ))
     }
 
-    if ("weight" %in% colnames(predictions)) {
-      weight_col_name <- paste0("weight_", rlang::hash(colnames(predictions)))
+    if ("weight" %in% colnames(model_outputs)) {
+      weight_col_name <- paste0("weight_", rlang::hash(colnames(model_outputs)))
       weights <- weights %>% dplyr::rename(!!weight_col_name := "weight")
     } else {
       weight_col_name <- "weight"
     }
 
-    predictions <- predictions %>%
+    model_outputs <- model_outputs %>%
       dplyr::left_join(weights, by = weight_by_cols)
 
-    if (agg_fun == "mean") agg_fun <- matrixStats::weightedMean
-    if (agg_fun == "median") agg_fun <- matrixStats::weightedMedian
+    if (is.character(agg_fun)) {
+      if (agg_fun == "mean") {
+        agg_fun <- matrixStats::weightedMean
+      } else if (agg_fun == "median") {
+        agg_fun <- matrixStats::weightedMedian
+      }
+    }
 
-    agg_args <- c(agg_args, list(x = "value", w = weight_col_name))
+    agg_args <- c(agg_args, list(x = quote(.data[["value"]]),
+                                 w = quote(.data[[weight_col_name]])))
   }
 
-  ensemble_predictions <- predictions %>%
-    dplyr::group_by(dplyr::across(dplyr::all_of(c(task_id_cols, output_type_col,
-                                                  output_id_col)))) %>%
+  group_by_cols <- c(task_id_cols, output_type_col, output_type_id_col)
+  ensemble_model_outputs <- model_outputs %>%
+    dplyr::group_by(dplyr::across(dplyr::all_of(group_by_cols))) %>%
     dplyr::summarize(value = do.call(agg_fun, args = agg_args)) %>%
-    dplyr::mutate(team_abbr = team_abbr, model_abbr = model_abbr,
-                  .before = 1) %>%
+    dplyr::mutate(model_id = model_id, .before = 1) %>%
     dplyr::ungroup()
 
-  return(ensemble_predictions)
+  # hubUtils::as_model_output_df(ensemble_model_outputs)
+
+  return(ensemble_model_outputs)
 }
--- tests/testthat/test-simple_ensemble.R
+++ tests/testthat/test-simple_ensemble.R
@@ -1,105 +1,120 @@
 library(matrixStats)
+library(dplyr)
 
-pred <- readr::read_csv("inst/example-data/2022-10-08-simple_hub-baseline.csv") %>%
-  mutate(team_abbr = "simple_hub", model_abbr = "baseline", .before = origin_date)
+pred <- read.csv(system.file("example-data/2022-10-08-simple_hub-baseline.csv",
+                             package = "hubEnsembles")) %>%
+  dplyr::mutate(model_id = "simple_hub-baseline", .before = origin_date)
 
-test_that("non-default column names are preserved in output data frame", {
+test_that("non-default columns are dropped from output", {
   output_names <- pred %>%
+    dplyr::mutate(extra_col_1 = "a", extra_col_2 = "a") %>%
     simple_ensemble(
-      agg_fun="mean",
-      model_abbr = "example",
-      output_type_col = "type", 
-      output_id_col = "type_id" 
+      task_id_cols = c("origin_date", "target", "horizon", "location")
     ) %>%
     names()
-  expect_equal(names(pred), output_names)
+
+  expect_equal(sort(names(pred)), sort(output_names))
 })
+
 
 test_that("invalid output type throws error", {
   expect_error(
     pred %>%
-      rename(output_type = type, output_id=type_id) %>%
-      mutate(output_type="sample") %>%
-      simple_ensemble(
-        agg_fun="mean",
-        model_abbr = "example"
-      )
+      dplyr::mutate(output_type = "sample") %>%
+      simple_ensemble()
   )
 })
+
 
 test_that("invalid method argument throws error", {
   expect_error(
-    pred %>%
-      rename(output_type = type, output_id=type_id) %>%
-      simple_ensemble(
-        agg_fun="linear pool",
-        model_abbr = "example"
-      )
+    simple_ensemble(pred, agg_fun="linear pool")
   )
 })
 
-test_that("medians and means correctly calculated", {
+
+test_that("(weighted) medians and means correctly calculated", {
   fdat <- expand.grid(
     stringsAsFactors = FALSE,
-    model_abbr = letters[1:4],
+    model_id = letters[1:4],
     location = c("222", "888"),
     horizon = 1, #week
     target = "inc death",
     target_date = as.Date("2021-12-25"),
     output_type = "quantile",
-    output_id = c(.1, .5, .9))
+    output_type_id = c(.1, .5, .9),
+    value = NA_real_)
 
-  fdat <- cbind(team_abbr = letters[23:26], fdat)
-  fdat$value[fdat$location == "222" & fdat$output_id == .1] <- v2.1 <- c(10, 30, 15, 20)
-  fdat$value[fdat$location == "222" & fdat$output_id == .5] <- v2.5 <- c(40, 40, 45, 50)
-  fdat$value[fdat$location == "222" & fdat$output_id == .9] <- v2.9 <- c(60, 70, 75, 80)
-  fdat$value[fdat$location == "888" & fdat$output_id == .1] <- v8.1 <- c(100, 300, 400, 250)
-  fdat$value[fdat$location == "888" & fdat$output_id == .5] <- v8.5 <- c(150, 325, 500, 300)
-  fdat$value[fdat$location == "888" & fdat$output_id == .9] <- v8.9 <- c(250, 350, 500, 350)
+  fdat$value[fdat$location == "222" & fdat$output_type_id == .1] <- v2.1 <-
+    c(10, 30, 15, 20)
+  fdat$value[fdat$location == "222" & fdat$output_type_id == .5] <- v2.5 <-
+    c(40, 40, 45, 50)
+  fdat$value[fdat$location == "222" & fdat$output_type_id == .9] <- v2.9 <-
+    c(60, 70, 75, 80)
+  fdat$value[fdat$location == "888" & fdat$output_type_id == .1] <- v8.1 <-
+    c(100, 300, 400, 250)
+  fdat$value[fdat$location == "888" & fdat$output_type_id == .5] <- v8.5 <-
+    c(150, 325, 500, 300)
+  fdat$value[fdat$location == "888" & fdat$output_type_id == .9] <- v8.9 <-
+    c(250, 350, 500, 350)
+
+  fweight2 <- data.frame(model_id = letters[1:4],
+                         location = "222",
+                         weight = 0.1 * (1:4))
+  fweight8 <- data.frame(model_id = letters[1:4],
+                         location = "888",
+                         weight = 0.1 * (4:1))
+  fweight <- bind_rows(fweight2, fweight8)
+
+  median_expected <- mean_expected <-
+    weighted_median_expected <- weighted_mean_expected <- data.frame(
+      model_id = "hub-ensemble",
+      location = rep(c("222", "888"), each = 3),
+      horizon = 1,
+      target = "inc death",
+      target_date = as.Date("2021-12-25"), 
+      output_type = "quantile", 
+      output_type_id = rep(c(.1, .5, .9), 2), 
+      value = NA_real_)
 
   median_vals <- sapply(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), median)
   mean_vals <- sapply(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), mean)
 
-  fweight <- tibble(model_abbr = letters[1:4], weight = 0.1*(1:4))
+  weighted_median_vals <- c(
+    sapply(list(v2.1, v2.5, v2.9),
+           matrixStats::weightedMedian,
+           w = fweight2$weight),
+    sapply(list(v8.1, v8.5, v8.9),
+           matrixStats::weightedMedian,
+           w = fweight8$weight))
+  weighted_mean_vals <- c(
+    sapply(list(v2.1, v2.5, v2.9),
+           matrixStats::weightedMean,
+           w = fweight2$weight),
+    sapply(list(v8.1, v8.5, v8.9),
+           matrixStats::weightedMean,
+           w = fweight8$weight))
 
-  weighted_median_vals <- map(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), weightedMedian, w = fweight$weight)
-  weighted_mean_vals <- map(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), weightedMean, w = fweight$weight)
+  median_expected$value <- median_vals
+  mean_expected$value <- mean_vals
+  weighted_mean_expected$value <- weighted_mean_vals
+  weighted_median_expected$value <- weighted_median_vals
 
-  median_actual <- simple_ensemble(
-    predictions = fdat, weights = NULL, agg_fun = "median", model_abbr = "median_ens")
-  mean_actual <- simple_ensemble(
-    predictions = fdat, weights = NULL, agg_fun = "mean", model_abbr = "mean_ens")
-    
-  weighted_median_actual <- simple_ensemble(
-    predictions = fdat, weights = fweight, agg_fun = "median", model_abbr = "weighted_median_ens")
-  weighted_mean_actual <- simple_ensemble(
-    predictions = fdat, weights = fweight, agg_fun = "mean", model_abbr = "weighted_mean_ens")
+  median_actual <- simple_ensemble(model_outputs = fdat, weights = NULL,
+                                   agg_fun = "median")
+  mean_actual <- simple_ensemble(model_outputs = fdat, weights = NULL,
+                                 agg_fun = "mean")
 
+  weighted_median_actual <- simple_ensemble(model_outputs = fdat,
+                                            weights = fweight,
+                                            agg_fun = "median")
+  weighted_mean_actual <- simple_ensemble(model_outputs = fdat,
+                                          weights = fweight,
+                                          agg_fun = "mean")
 
-  median_expected <- mean_expected <- weighted_median_expected <- weighted_mean_expected <- tibble::tibble(
-    team_abbr = "Hub",
-    model_abbr = NA,
-    location = rep(c("222", "888"), each = 3), 
-    horizon = 1, #week
-    target = "inc death",     
-    target_date = as.Date("2021-12-25"), 
-    output_type = "quantile", 
-    output_id = rep(c(.1, .5, .9), 2), 
-    value = 0)
+  expect_equal(as.data.frame(median_actual), median_expected)
+  expect_equal(as.data.frame(mean_actual), mean_expected)
 
-  median_expected$value <- sapply(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), median)
-  median_expected$model_abbr <- "median_ens"
-  mean_expected$value <- sapply(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), mean)
-  mean_expected$model_abbr <- "mean_ens"
-
-  weighted_mean_expected$value <- map_dbl(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), weightedMean, w = fweight$weight)
-  weighted_mean_expected$model_abbr <- "weighted_mean_ens"
-  weighted_median_expected$value <- map_dbl(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), weightedMedian, w = fweight$weight)
-  weighted_median_expected$model_abbr <- "weighted_median_ens"
-
-  expect_equal(median_actual, median_expected)
-  expect_equal(mean_actual, mean_expected)
-  
-  expect_equal(weighted_median_actual, weighted_median_expected)
-  expect_equal(weighted_mean_actual, weighted_mean_expected)
+  expect_equal(as.data.frame(weighted_median_actual), weighted_median_expected)
+  expect_equal(as.data.frame(weighted_mean_actual), weighted_mean_expected)
 })
