e856a16a3a4cecd79cb0048dd01f5d7ebd3689f0
Li Shandross <57642277+lshandross@users.noreply.github.com>
2023-12-15 15:36:00 -0500

    bold best results in table with format_cells function

---
+++ manuscript/format_cells.R
@@ -0,0 +1,21 @@
+# from here https://stackoverflow.com/questions/28166168/how-to-change-fontface-bold-italics-for-a-cell-in-a-kable-table-in-rmarkdown?noredirect=1&lq=1
+format_cells <- function(df, rows ,cols, value = c("italics", "bold", "strikethrough")){
+
+  # select the correct markup
+  # one * for italics, two ** for bold
+  map <- setNames(c("*", "**", "~~"), c("italics", "bold", "strikethrough"))
+  markup <- map[value]  
+
+  for (r in rows){
+    for(c in cols){
+
+      # Make sure values are not factors
+      df[[c]] <- as.character( df[[c]])
+
+      # Update formatting
+      df[r, c] <- paste0(markup, df[r, c], markup)
+    }
+  }
+
+  return(df)
+}
