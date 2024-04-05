d8c8911c2100ee7ef58927bb93b42e314ef154b4
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-04-05 11:08:37 -0400

    Fixes tests directory linting issues (#103)

--- tests/testthat/test-simple_ensemble.R
+++ tests/testthat/test-simple_ensemble.R
@@ -3,34 +3,33 @@ library(matrixStats)
 library(dplyr)
 
 # set up simple data for test cases
-model_outputs <- expand.grid(
-  stringsAsFactors = FALSE,
-  model_id = letters[1:4],
-  location = c("222", "888"),
-  horizon = 1, #week
-  target = "inc death",
-  target_date = as.Date("2021-12-25"),
-  output_type = "quantile",
-  output_type_id = c(.1, .5, .9),
-  value = NA_real_)
+model_outputs <- expand.grid(stringsAsFactors = FALSE,
+                             model_id = letters[1:4],
+                             location = c("222", "888"),
+                             horizon = 1, #week
+                             target = "inc death",
+                             target_date = as.Date("2021-12-25"),
+                             output_type = "quantile",
+                             output_type_id = c(.1, .5, .9),
+                             value = NA_real_)
 
-v2.1 <- model_outputs$value[model_outputs$location == "222" &
-                            model_outputs$output_type_id == .1] <-
+v2_1 <- model_outputs$value[model_outputs$location == "222" &
+                              model_outputs$output_type_id == .1] <-
   c(10, 30, 15, 20)
-v2.5 <- model_outputs$value[model_outputs$location == "222" &
-                            model_outputs$output_type_id == .5] <-
+v2_5 <- model_outputs$value[model_outputs$location == "222" &
+                              model_outputs$output_type_id == .5] <-
   c(40, 40, 45, 50)
-v2.9 <- model_outputs$value[model_outputs$location == "222" &
-                            model_outputs$output_type_id == .9] <-
+v2_9 <- model_outputs$value[model_outputs$location == "222" &
+                              model_outputs$output_type_id == .9] <-
   c(60, 70, 75, 80)
-v8.1 <- model_outputs$value[model_outputs$location == "888" &
-                            model_outputs$output_type_id == .1] <-
+v8_1 <- model_outputs$value[model_outputs$location == "888" &
+                              model_outputs$output_type_id == .1] <-
   c(100, 300, 400, 250)
-v8.5 <- model_outputs$value[model_outputs$location == "888" &
-                            model_outputs$output_type_id == .5] <-
+v8_5 <- model_outputs$value[model_outputs$location == "888" &
+                              model_outputs$output_type_id == .5] <-
   c(150, 325, 500, 300)
-v8.9 <- model_outputs$value[model_outputs$location == "888" &
-                            model_outputs$output_type_id == .9] <-
+v8_9 <- model_outputs$value[model_outputs$location == "888" &
+                              model_outputs$output_type_id == .9] <-
   c(250, 350, 500, 350)
 
 fweight2 <- data.frame(model_id = letters[1:4],
@@ -61,146 +60,143 @@ test_that("invalid method argument throws error", {
 })
 
 
-test_that("sorted unique output_type_ids are identical in the component model outputs and the resulting ensemble model outputs", {
-  quantile_outputs <- expand.grid(
-    stringsAsFactors = FALSE,
-    model_id = letters[1:4],
-    location = c("222", "888"),
-    horizon = 1, #week
-    target = "inc death",
-    target_date = as.Date("2021-12-25"),
-    output_type = "quantile",
-    output_type_id = c(.1, .5, .9),
-    value = NA_real_)
+test_that("component model outputs and resulting ensemble model outputs have identical sorted unique output type ids", {
+  quantile_outputs <- expand.grid(stringsAsFactors = FALSE,
+                                  model_id = letters[1:4],
+                                  location = c("222", "888"),
+                                  horizon = 1, #week
+                                  target = "inc death",
+                                  target_date = as.Date("2021-12-25"),
+                                  output_type = "quantile",
+                                  output_type_id = c(.1, .5, .9),
+                                  value = NA_real_)
 
   quantile_outputs$value[quantile_outputs$location == "222" &
-                            quantile_outputs$output_type_id == .1] <-
+                           quantile_outputs$output_type_id == .1] <-
     c(10, 30, 15, 20)
   quantile_outputs$value[quantile_outputs$location == "222" &
-                            quantile_outputs$output_type_id == .5] <-
+                           quantile_outputs$output_type_id == .5] <-
     c(40, 40, 45, 50)
   quantile_outputs$value[quantile_outputs$location == "222" &
-                            quantile_outputs$output_type_id == .9] <-
+                           quantile_outputs$output_type_id == .9] <-
     c(60, 70, 75, 80)
   quantile_outputs$value[quantile_outputs$location == "888" &
-                            quantile_outputs$output_type_id == .1] <-
+                           quantile_outputs$output_type_id == .1] <-
     c(100, 300, 400, 250)
   quantile_outputs$value[quantile_outputs$location == "888" &
-                            quantile_outputs$output_type_id == .5] <-
+                           quantile_outputs$output_type_id == .5] <-
     c(150, 325, 500, 300)
   quantile_outputs$value[quantile_outputs$location == "888" &
-                            quantile_outputs$output_type_id == .9] <-
+                           quantile_outputs$output_type_id == .9] <-
     c(250, 350, 500, 350)
 
   expected_output_type_ids <- sort(unique(quantile_outputs$output_type_id))
-    
-  actual_output_type_ids <- simple_ensemble(quantile_outputs, weights = NULL,
-                                          weights_col_name = NULL,
-                                          model_id = "hub-ensemble",
-                                          task_id_cols = NULL) |>
-                              dplyr::pull(output_type_id) |>
-                              unique() |>
-                              sort()
-                      
+
+  actual_output_type_ids <- quantile_outputs |>
+    simple_ensemble(quantile_outputs, weights = NULL,
+                    weights_col_name = NULL,
+                    model_id = "hub-ensemble",
+                    task_id_cols = NULL) |>
+    dplyr::pull(output_type_id) |>
+    unique() |>
+    sort()
+
   expect_equal(expected_output_type_ids, actual_output_type_ids)
 })
 
 test_that("group_by(output_type_id) produces expected results", {
-  explicit_outputs <- expand.grid(
-    stringsAsFactors = FALSE,
-    model_id = letters[1:4],
-    location = c("222", "888"),
-    horizon = 1, #week
-    target = "inc death",
-    target_date = as.Date("2021-12-25"),
-    output_type = "quantile",
-    output_type_id = c(.025, .1, .25, .75, .9, .975),
-    value = NA_real_)
-  
+  explicit_outputs <- expand.grid(stringsAsFactors = FALSE,
+                                  model_id = letters[1:4],
+                                  location = c("222", "888"),
+                                  horizon = 1, #week
+                                  target = "inc death",
+                                  target_date = as.Date("2021-12-25"),
+                                  output_type = "quantile",
+                                  output_type_id = c(.025, .1, .25, .75, .9, .975),
+                                  value = NA_real_)
+
   explicit_outputs$value[explicit_outputs$location == "222" &
-                            explicit_outputs$output_type_id == .025] <-
+                           explicit_outputs$output_type_id == .025] <-
     c(4, 12, 6, 8)
   explicit_outputs$value[explicit_outputs$location == "222" &
-                            explicit_outputs$output_type_id == .100] <-
+                           explicit_outputs$output_type_id == .100] <-
     c(10, 30, 15, 20)
   explicit_outputs$value[explicit_outputs$location == "222" &
-                            explicit_outputs$output_type_id == .250] <-
+                           explicit_outputs$output_type_id == .250] <-
     c(20, 40, 25, 30)
   explicit_outputs$value[explicit_outputs$location == "222" &
-                            explicit_outputs$output_type_id == .750] <-
+                           explicit_outputs$output_type_id == .750] <-
     c(50, 50, 55, 60)
   explicit_outputs$value[explicit_outputs$location == "222" &
-                            explicit_outputs$output_type_id == .900] <-
+                           explicit_outputs$output_type_id == .900] <-
     c(60, 70, 75, 80)
   explicit_outputs$value[explicit_outputs$location == "222" &
-                            explicit_outputs$output_type_id == .975] <-
+                           explicit_outputs$output_type_id == .975] <-
     c(70, 80, 85, 90)
   explicit_outputs$value[explicit_outputs$location == "888" &
-                            explicit_outputs$output_type_id == .025] <-
+                           explicit_outputs$output_type_id == .025] <-
     c(40, 120, 160, 100)
   explicit_outputs$value[explicit_outputs$location == "888" &
-                            explicit_outputs$output_type_id == .100] <-
+                           explicit_outputs$output_type_id == .100] <-
     c(100, 300, 400, 250)
   explicit_outputs$value[explicit_outputs$location == "888" &
-                            explicit_outputs$output_type_id == .250] <-
+                           explicit_outputs$output_type_id == .250] <-
     c(150, 325, 475, 300)
   explicit_outputs$value[explicit_outputs$location == "888" &
-                            explicit_outputs$output_type_id == .750] <-
+                           explicit_outputs$output_type_id == .750] <-
     c(200, 325, 500, 325)
   explicit_outputs$value[explicit_outputs$location == "888" &
-                            explicit_outputs$output_type_id == .900] <-
+                           explicit_outputs$output_type_id == .900] <-
     c(250, 350, 500, 350)
   explicit_outputs$value[explicit_outputs$location == "888" &
-                            explicit_outputs$output_type_id == .975] <-
+                           explicit_outputs$output_type_id == .975] <-
     c(350, 450, 550, 450)
-  
-  intervals = c(.50, .80, .95)
+
+  intervals <- c(.50, .80, .95)
   implicit_outputs <- explicit_outputs
-  implicit_outputs$output_type_id <- sort(rep(c((1-intervals)/2, 1-(1-intervals)/2), 8))
+  implicit_outputs$output_type_id <- sort(rep(c((1 - intervals) / 2, 1 - (1 - intervals) / 2), 8))
 
   explicit_ensemble <- simple_ensemble(explicit_outputs, weights = NULL,
-                                          weights_col_name = NULL,
-                                          model_id = "hub-ensemble",
-                                          task_id_cols = NULL) 
-    
+                                       weights_col_name = NULL,
+                                       model_id = "hub-ensemble",
+                                       task_id_cols = NULL)
+
   implicit_ensemble <- simple_ensemble(implicit_outputs, weights = NULL,
-                                          weights_col_name = NULL,
-                                          model_id = "hub-ensemble",
-                                          task_id_cols = NULL) 
-                                          
+                                       weights_col_name = NULL,
+                                       model_id = "hub-ensemble",
+                                       task_id_cols = NULL)
+
   expect_equal(explicit_ensemble, implicit_ensemble)
 })
 
 
 test_that("(weighted) medians and means correctly calculated", {
   median_expected <- mean_expected <-
-    weighted_median_expected <- weighted_mean_expected <- data.frame(
-      model_id = "hub-ensemble",
-      location = rep(c("222", "888"), each = 3),
-      horizon = 1,
-      target = "inc death",
-      target_date = as.Date("2021-12-25"),
-      output_type = "quantile",
-      output_type_id = rep(c(.1, .5, .9), 2),
-      value = NA_real_)
+    weighted_median_expected <- weighted_mean_expected <-
+    data.frame(model_id = "hub-ensemble",
+               location = rep(c("222", "888"), each = 3),
+               horizon = 1,
+               target = "inc death",
+               target_date = as.Date("2021-12-25"),
+               output_type = "quantile",
+               output_type_id = rep(c(.1, .5, .9), 2),
+               value = NA_real_)
 
-  median_vals <- sapply(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), median)
-  mean_vals <- sapply(list(v2.1, v2.5, v2.9, v8.1, v8.5, v8.9), mean)
+  median_vals <- sapply(list(v2_1, v2_5, v2_9, v8_1, v8_5, v8_9), median)
+  mean_vals <- sapply(list(v2_1, v2_5, v2_9, v8_1, v8_5, v8_9), mean)
 
-  weighted_median_vals <- c(
-    sapply(list(v2.1, v2.5, v2.9),
-           matrixStats::weightedMedian,
-           w = fweight2$weight),
-    sapply(list(v8.1, v8.5, v8.9),
-           matrixStats::weightedMedian,
-           w = fweight8$weight))
-  weighted_mean_vals <- c(
-    sapply(list(v2.1, v2.5, v2.9),
-           matrixStats::weightedMean,
-           w = fweight2$weight),
-    sapply(list(v8.1, v8.5, v8.9),
-           matrixStats::weightedMean,
-           w = fweight8$weight))
+  weighted_median_vals <- c(sapply(list(v2_1, v2_5, v2_9),
+                                   matrixStats::weightedMedian,
+                                   w = fweight2$weight),
+                            sapply(list(v8_1, v8_5, v8_9),
+                                   matrixStats::weightedMedian,
+                                   w = fweight8$weight))
+  weighted_mean_vals <- c(sapply(list(v2_1, v2_5, v2_9),
+                                 matrixStats::weightedMean,
+                                 w = fweight2$weight),
+                          sapply(list(v8_1, v8_5, v8_9),
+                                 matrixStats::weightedMean,
+                                 w = fweight8$weight))
 
   median_expected$value <- median_vals
   mean_expected$value <- mean_vals
@@ -230,18 +226,14 @@ test_that("(weighted) medians and means correctly calculated", {
 
 
 test_that("(weighted) medians and means work with alternate name for weights columns", {
-  weighted_median_actual <- simple_ensemble(
-    model_outputs = model_outputs,
-    weights = fweight %>%
-      dplyr::rename(w = weight),
-    weights_col_name = "w",
-    agg_fun = "median")
-  weighted_mean_actual <- simple_ensemble(
-    model_outputs = model_outputs,
-    weights = fweight %>%
-      dplyr::rename(w = weight),
-    weights_col_name = "w",
-    agg_fun = "mean")
+  weighted_median_actual <- model_outputs |>
+    simple_ensemble(weights = fweight %>% dplyr::rename(w = weight),
+                    weights_col_name = "w",
+                    agg_fun = "median")
+  weighted_mean_actual <- model_outputs |>
+    simple_ensemble(weights = fweight %>% dplyr::rename(w = weight),
+                    weights_col_name = "w",
+                    agg_fun = "mean")
 
   weighted_median_expected <- simple_ensemble(model_outputs = model_outputs,
                                               weights = fweight,
