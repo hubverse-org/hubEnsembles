b4ed6ce405b3bfcdef51f1b6ed208f2eaa1ce417
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-12-16 17:02:49 -0500

    Change `compound_taskid_set = NULL` to mean the values of all task ID vars display dependence
    Co-authored-by: Evan Ray <elray1@users.noreply.github.com>

--- R/linear_pool.R
+++ R/linear_pool.R
@@ -10,9 +10,11 @@
 #'   details of how to estimate a quantile function from provided quantile levels
 #'   and quantile values for `output_type` `"quantile"`.
 #' @param compound_taskid_set `character` vector of the compound task ID variable
-#'   set. NULL means all columns' values display dependency while equality to
-#'   task_id_cols means that none of the columns' values are dependent.
-#'   Defaults to NULL, in which case the task id variables are used.
+#'   set. This argument is only relevant for `output_type` `"sample"`. NULL means that
+#'   samples are from a multivariate joint distribution across all levels of all task id variables,
+#'   while equality to `task_id_cols` means that the samples are from separate univariate
+#'   distributions for each individual prediction task.
+#'   
 #' @param n_output_samples `numeric` that specifies how many sample forecasts to
 #'   return per unique combination of task IDs. Currently the only supported value
 #'   is NULL, in which case all provided component model samples are collected and
@@ -62,7 +64,7 @@ linear_pool <- function(model_out_tbl, weights = NULL,
                         weights_col_name = "weight",
                         model_id = "hub-ensemble",
                         task_id_cols = NULL,
-                        compound_taskid_set = NULL,
+                        compound_taskid_set,
                         n_samples = 1e4,
                         n_output_samples = NULL,
                         ...) {
